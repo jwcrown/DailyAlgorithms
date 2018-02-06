0173dbf68076047b578d5ccab29b93d994644111
Joshua Crown <joshuacrown@gmail.com>
2018-02-06 15:11:50 -0800

    Added SLL contains and addFront

---
+++ SLL.js
@@ -0,0 +1,37 @@
+function Node(val){
+    this.val = val;
+    this.next = null;
+}
+
+function SSL(){
+    this.head = null;
+
+    //methods for linked list
+    this.addFront = function(val){
+        var newNode = new Node(val);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    this.contains = function(val){
+        if (!this.head){
+            return false;
+        }
+        var runner = this.head;
+        while (runner){
+            if (runner.val == val){
+                return true;
+            }
+            runner = runner.next;
+        }
+        return false;
+    }
+}
+
+var mySSL = new SSL();
+mySSL.addFront(2);
+mySSL.addFront(3);
+mySSL.addFront(50);
+mySSL.addFront(13);
+mySSL.addFront(9);
+console.log(mySSL.contains(3));
