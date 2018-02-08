2546b9713a843f094383f1c65c0943ce954fc2ef
Joshua Crown <joshuacrown@gmail.com>
2018-02-08 11:39:59 -0800

    added removeMinimum

--- SLQueue.js
+++ SLQueue.js
@@ -79,24 +79,56 @@ function SLQueue(){
 function compareQueues(queue1, queue2){
     var runner1 = queue1.head;
     var runner2 = queue2.head;
-    while (runner1 && runner2){
+    while (runner1 && runner2){//compares both queues nodes and returns false if not a match
         if (runner1.val != runner2.val){
             return false;
         }
         runner1 = runner1.next;
         runner2 = runner2.next;
     }
-    if (!runner1 && !runner2){
+    if (!runner1 && !runner2){//both queues should finish at same time if matching
         return true;
     }
-    return false;
+    return false;//queues did not finish at same time
+
+}
+
+function removeMinimum(queue){
+    if (!queue.head){
+        return false;
+    }
+    var runner = queue.head;
+    var min = queue.head;
+    var parent = null;//parent of current minimum
+    while (runner.next){
+        if (runner.next.val <= min.val){//looks ahead one and compares to min
+            min = runner.next;
+            parent = runner;//updating parent to one before min
+        }
+        runner = runner.next;//iterates through queue
+    }
+    if(!parent){//for case when min is first node
+        queue.head = queue.head.next;
+        if (queue.tail == min){//updates tail if last node was removed
+            queue.tail = parent;
+        }
+    }
+    else{
+        parent.next = parent.next.next;
+        if (queue.tail == min){//updates tail if last node was removed
+            queue.tail = parent;
+        }
+    }
+    return min.val;
 }
 
 var myQueue1 = new SLQueue();
-myQueue1.enqueue(3);
+myQueue1.enqueue(11);
+myQueue1.enqueue(1);
 myQueue1.enqueue(2);
-var myQueue2 = new SLQueue();
-myQueue2.enqueue(3);
-myQueue2.enqueue(3);
+myQueue1.enqueue(2);
+myQueue1.enqueue(1);
 
-console.log(compareQueues(myQueue1, myQueue2))
+console.log(removeMinimum(myQueue1))
+console.log(myQueue1.queueSize());
+console.log(myQueue1)
