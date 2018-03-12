e9956a67d63fa68289dee0e5ef592f2dee5bd30f
Joshua Crown <joshuacrown@gmail.com>
2018-03-12 10:59:46 -0700

    added recursiveSigma

---
+++ RecursiveSigma.js
@@ -0,0 +1,11 @@
+function recursiveSigma(sigmaNum){
+    sigmaNum = Math.floor(sigmaNum);
+    if(sigmaNum < 1){
+        return 0;
+    }
+    if(sigmaNum == 1){
+        return sigmaNum;
+    }
+    return sigmaNum + recursiveSigma(sigmaNum - 1);
+}
+console.log(recursiveSigma(5))
